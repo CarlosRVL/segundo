76b33b6dc9adb32b6e6217a5ef387ee041908de3
CarlosRVL <37228433+CarlosRVL@users.noreply.github.com>
2024-06-03 23:54:09 +0200

    Create proyecto-alfa.py

---
+++ proyecto-alfa.py
@@ -0,0 +1,10 @@
+"""
+Esqueleto de un programa que haga algo, con el obtetivo de estudiar como
+se debe dise~ar un software (e implementar).
+Se obtendran parametros en linea de comandos, tambien en modo interactivo
+Se revisaran los parametros para tomar algunas decisiones. 
+El procesamiento se realizara con una clase principal y otras auxiliares
+Utilizaremos un fichero a parte para los mensajes de comunicación con el 
+usuario
+Utilizaremos un fichero de configuracion
+"""
